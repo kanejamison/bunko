835ea3fea51748e33f2138623c0ed043fa7400c0
Kane <918780+kanejamison@users.noreply.github.com>
2025-11-09 12:04:23 -0800

    Change controller methods to match model structure

--- lib/bunko.rb
+++ lib/bunko.rb
@@ -5,19 +5,10 @@ require_relative "bunko/configuration"
 require_relative "bunko/models/post_methods"
 require_relative "bunko/models/post_type_methods"
 require_relative "bunko/models/acts_as"
-require_relative "bunko/controller"
+require_relative "bunko/controllers/collection"
+require_relative "bunko/controllers/acts_as"
 require_relative "bunko/railtie" if defined?(Rails::Railtie)
 
 module Bunko
   class Error < StandardError; end
-end
-
-# Extend ActionController::Base with bunko_collection method
-if defined?(ActionController::Base)
-  ActionController::Base.class_eval do
-    def self.bunko_collection(collection_name, **options)
-      include Bunko::Controller
-      bunko_collection(collection_name, **options)
-    end
-  end
 end
