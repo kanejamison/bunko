9af80464342fdb017751997fb788bc0c0ab9f294
Kane <918780+kanejamison@users.noreply.github.com>
2025-11-09 17:49:55 -0800

    add simplecov

--- Gemfile
+++ Gemfile
@@ -11,3 +11,7 @@ gem "rake", "~> 13.0"
 gem "minitest", "~> 5.16"
 
 gem "standard", "~> 1.3"
+
+group :test do
+  gem "simplecov", require: false
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start do
+  add_filter "/test/"
+  add_filter "/vendor/"
+  add_group "Controllers", "lib/bunko/controllers"
+  add_group "Models", "lib/bunko/models"
+  add_group "Configuration", "lib/bunko/configuration.rb"
+  add_group "Routing", "lib/bunko/routing"
+  add_group "Generators", "lib/generators"
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 # Load the dummy Rails app
